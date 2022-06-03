fcbd3624a64156a3907423ef1596bab1653b7683
Gustavo da Silva Rodrigues <gustavo.rodrigues@maxmilhas.com.br>
2022-06-03 11:02:40 -0300

    feat: supports renewing connect on error (#6)
    * feat: supports renewing connect on error
    
    When gRPC has some network error it needs to
    discard the old connection and add a new connection, especially in
    this lib, because it uses Proxy to resolve instances,
    and Proxy makes a trap to not able external application create new
    instances by itself.
    
    * chore: preparing semantic-release to run only on master
    
    * chore: force exiting on tests
    
    Some task is making the node process to not finish
    
    Co-authored-by: Thiago Oliveira Santos <tos.oliveira@gmail.com>

--- src/client-pool.ts
+++ src/client-pool.ts
@@ -1,9 +1,31 @@
+import { Client } from '@grpc/grpc-js';
+import { ClientConfig } from './client-config';
+
 /* eslint-disable @typescript-eslint/no-shadow */
 type Con = Object;
 interface ConnectionPool {
 	position: number;
 	connections: any[];
+	config: ClientConfig;
+	factory: Function;
 	proxy?: any;
+}
+
+function factoryPoolProxy(connectionPool: ConnectionPool): ConnectionPool {
+	return new Proxy(connectionPool.connections[0], {
+		get(_target, name) {
+			connectionPool.position =
+				(connectionPool.position + 1) % connectionPool.connections.length;
+			const client = connectionPool.connections[connectionPool.position];
+			client.poolPosition = connectionPool.position;
+			if (name in client) {
+				if (typeof client[name as keyof Con] === 'function') {
+					return client[name as keyof Con].bind(client);
+				}
+				return client[name as keyof Con];
+			}
+		},
+	});
 }
 
 export class ClientPool {
@@ -11,36 +33,52 @@ export class ClientPool {
 	public static create<T>(
 		alias: string,
 		size: number,
+		config: ClientConfig,
 		createClient: () => T,
 	): T {
 		let connectionPool: ConnectionPool | undefined =
 			ClientPool.clientsPools.get(alias);
 		if (!connectionPool) {
+			const client = createClient();
+			(client as any).poolPosition = -1;
 			connectionPool = {
 				position: -1,
-				connections: [createClient()],
+				config,
+				factory: createClient,
+				connections: [client],
 			};
 			ClientPool.clientsPools.set(alias, connectionPool);
 
-			connectionPool.proxy = new Proxy(connectionPool.connections[0], {
-				get(_target, name) {
-					connectionPool!.position =
-						(connectionPool!.position + 1) % connectionPool!.connections.length;
-					const client = connectionPool!.connections[connectionPool!.position];
-					if (name in client) {
-						if (typeof client[name as keyof Con] === 'function') {
-							return client[name as keyof Con].bind(client);
-						}
-						return client[name as keyof Con];
-					}
-				},
-			});
+			connectionPool.proxy = factoryPoolProxy(connectionPool);
 		}
 
 		while (connectionPool.connections.length < size) {
-			connectionPool.connections.push(createClient());
+			this.addConnection(alias, createClient);
 		}
 
 		return connectionPool.proxy;
+	}
+
+	public static addConnection<T>(alias: string, createClient: () => T): T {
+		const pool = ClientPool.clientsPools.get(alias);
+		if (!pool) {
+			throw new Error(`Not found pool ${alias} to add a new connection`);
+		}
+		const client = createClient() as any;
+
+		pool.connections.push(client);
+		return pool.proxy;
+	}
+
+	public static renewConnect<T>(alias: string, client: Client): T {
+		const position = (client as any).poolPosition;
+		const pool = ClientPool.clientsPools.get(alias);
+		if (!pool) {
+			throw new Error(`Not found pool ${alias} to renew connection`);
+		}
+		client.close();
+		const newClient = pool.factory();
+		pool.connections[position] = newClient;
+		return pool.proxy;
 	}
 }
